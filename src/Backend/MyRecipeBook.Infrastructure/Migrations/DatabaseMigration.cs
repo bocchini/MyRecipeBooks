e8e594aefbb01dfb92e88f50b8cbe54544759bf9
Americo Bocchini <albocchini@gmail.com>
2025-05-12 17:02:35 -0300

    Criação do database

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace MyRecipeBook.Infrastructure.Migrations;
+public static class DatabaseMigration
+{
+    public static void Migrate(string connectionString)
+    {
+        EnsureDataBaseCreate(connectionString);
+    }
+
+    private static void EnsureDataBaseCreate(string connectionString)
+    {
+        var connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+        var databaseName = connectionStringBuilder.Database;
+
+        connectionStringBuilder.Remove("Database");
+
+        using var dbConnection = new MySqlConnection(connectionStringBuilder.ConnectionString);
+        var parameters = new DynamicParameters();
+        parameters.Add("name", databaseName);
+        var records = dbConnection.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @name", parameters);
+
+        if(records.Any() == false)
+            dbConnection.Execute($"CREATE DATABASE {databaseName}");
+    }
+}
